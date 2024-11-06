efad8989e26ea5c342ce06c7fc5e23972693fee7
Yusuf <68289164+YusufSenyuz@users.noreply.github.com>
2024-11-06 03:08:01 +0300

    Upload file code.py

--- code.py
+++ code.py
@@ -1,7 +1,5 @@
 # CS 421 Programming Assignment 1 - PseudoGit
-
 def greet_the_student():
-    return "Hello World"
-
+    return "21903105"
 if(__name__ == "__main__"):
     print(greet_the_student())
